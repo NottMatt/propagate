c436905c7d44f3f5ff016a32aa363e063b090241
NottMatt <mattdtie@gmail.com>
2023-03-11 15:33:32 +0000

    profile

--- javascript/gate-manager.js
+++ javascript/gate-manager.js
@@ -10,3 +10,19 @@ const ledRBtn = document.getElementById("led_r-btn");
 const ledGBtn = document.getElementById("led_g-btn");
 const ledBBtn = document.getElementById("led_b-btn");
 const clockBtn = document.getElementById("clock-btn");
+
+andBtn.addEventListener("click", buttonActivity);
+nandBtn.addEventListener("click", buttonActivity);
+orBtn.addEventListener("click", buttonActivity);
+norBtn.addEventListener("click", buttonActivity);
+xorBtn.addEventListener("click", buttonActivity);
+xnorBtn.addEventListener("click", buttonActivity);
+notBtn.addEventListener("click", buttonActivity);
+ledRBtn.addEventListener("click", buttonActivity);
+ledGBtn.addEventListener("click", buttonActivity);
+ledBBtn.addEventListener("click", buttonActivity);
+clockBtn.addEventListener("click", buttonActivity);
+
+function buttonActivity() {
+    console.log("click!");
+}
