2c68b4503f24e8c047bfd81894850b0be71e5177
bharper <bharper19@georgefox.edu>
2023-03-11 20:20:11 -0800

    done

--- javascript/search-utils.js
+++ javascript/search-utils.js
@@ -1,11 +1,9 @@
 // Find all the posts that are related to inputted text value
 function searchPosts()
 {
-    console.log("Is happening.")
     var textvalue = document.getElementById("searchbar").value;
-    console.log(textvalue)
 
-    fetch("search/" + new URLSearchParams({"q": textvalue}))
+    fetch("search?" + new URLSearchParams({"q": textvalue}))
         .then((response) => response.json())
         .then((data) => console.log(data));
 }
