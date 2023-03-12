bedcb2e842df450b0b8418fe0ae3707384a67c4b
NottMatt <mattdtie@gmail.com>
2023-03-12 02:32:48 +0000

    something

--- javascript/login-state.js
+++ javascript/login-state.js
@@ -1,9 +1,10 @@
 fetch("/is-logged-in", { 
     method: 'GET'
   })
-      .then((response) => response.json())
+      .then((response) => {response.json()})
       .then((data) => 
-      {loginBtn = document.getElementById("login-btn");
+      {
+        loginBtn = document.getElementById("login-btn");
       if (data == "true") {
         loginBtn.textContent = "Logout"
       }
