deb280e4d723cd68cc441c5b9db0b72234313eb8
bharper <bharper19@georgefox.edu>
2023-03-11 21:51:24 -0800

    nevermind

--- javascript/editor.js
+++ javascript/editor.js
@@ -14,6 +14,7 @@ function loadSavedFile() {
         .then((response) => response.json())
         .then((data) =>
         {
+            console.log(((data['content'])['value']))
             document.getElementById("default-model").value = ((data['content'])['value']);
             load();
         });
