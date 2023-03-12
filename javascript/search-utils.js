4d66de2c12344a6787a5d23b92573d6a84790615
bharper <bharper19@georgefox.edu>
2023-03-11 20:26:09 -0800

    added update thing

--- javascript/search-utils.js
+++ javascript/search-utils.js
@@ -5,5 +5,10 @@ function searchPosts()
 
     fetch("search?" + new URLSearchParams({"q": textvalue}))
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) =>
+        {
+            data.foreach(
+                document.getElementById("feedBody").innerHTML += "<div class=\"feed-post\"><h2>Testing name</h2><h3>Testing desc</h3></div>"
+            )
+        });
 }
